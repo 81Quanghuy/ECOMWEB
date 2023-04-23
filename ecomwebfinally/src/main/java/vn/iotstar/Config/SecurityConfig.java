3fbf14dc0e9b32109be3453a7e2698284c7273d4
Bùi Đặng Quốc Chí <103109218+1905Chi@users.noreply.github.com>
2023-04-23 20:21:22 +0700

    update SecurityConfig to fix Missing

--- ecomwebfinally/src/main/java/vn/iotstar/Config/SecurityConfig.java
+++ ecomwebfinally/src/main/java/vn/iotstar/Config/SecurityConfig.java
@@ -1,37 +1,16 @@
-//package vn.iotstar.Config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//
-//import vn.iotstar.service.Impl.CustomOAuth2UserService;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-// 
-//     
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//            .antMatchers("/", "/login", "/oauth/**").permitAll()
-//            .anyRequest().authenticated()
-//            .and()
-//            .formLogin().permitAll()
-//            .and()
-//            .oauth2Login()
-//                .loginPage("/login")
-//                .userInfoEndpoint()
-//                    .userService((OAuth2UserService<OAuth2UserRequest, OAuth2User>) oauthUserService);
-//    }
-//     
-//    @Autowired
-//    private CustomOAuth2UserService oauthUserService;
-//     
-// 
-//}
+package vn.iotstar.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin();
+    }
+}
